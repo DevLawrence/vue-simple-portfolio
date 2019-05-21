93c0f983a25689d1d81976cc9c0b55a3d4bdbeb3
Lawrence <lawrencekkyalo@gmail.com>
2019-05-21 09:09:55 +0300

    Update default.vue

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -2,9 +2,20 @@
 
 <template>
   <div>
+    <navbar></navbar>
     <nuxt/>
   </div>
 </template>
+
+<script>
+  import Navbar from '../components/Navbar'
+
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
 
 <style>
   html {
