5e665ad4fae8d7546b2b5fed6dc40d43e2ea58b6
Lawrence <lawrencekkyalo@gmail.com>
2019-05-21 09:03:15 +0300

    Update nuxt.config.js

--- portfolio/nuxt.config.js
+++ portfolio/nuxt.config.js
@@ -10,7 +10,9 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'server side rendered vue app with nuxt.js' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700' }
     ]
   },
   /*
